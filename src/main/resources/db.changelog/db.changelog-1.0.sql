ea066634851f0dfb99457b658ae58f467b358430
oly <olgaivanova.19@icloud.com>
2026-08-22 11:31:20 +0300

    22.08

---
+++ src/main/resources/db.changelog/db.changelog-1.0.sql
@@ -0,0 +1,34 @@
+--liquibase formatted sql
+
+--changeset oivanova:1
+CREATE TABLE player (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        username VARCHAR(255) UNIQUE NOT NULL
+);
+
+--changeset oivanova:2
+CREATE TABLE player_account (
+                                account_number UUID PRIMARY KEY,
+                                balance NUMERIC(38,2),
+                                player_id INT REFERENCES player(id)
+);
+
+--changeset oivanova:3
+CREATE TABLE player_audit (
+                              id SERIAL PRIMARY KEY,
+                              action VARCHAR(255) NOT NULL,
+                              date_time TIMESTAMP,
+                              player_id INT REFERENCES player(id)
+);
+
+--changeset oivanova:4
+CREATE TABLE transaction (
+                             id SERIAL PRIMARY KEY,
+                             created_date TIMESTAMP,
+                             player_account_from UUID REFERENCES player_account(account_number),
+                             player_account_to UUID REFERENCES player_account(account_number),
+                             sum NUMERIC(38,2),
+                             type VARCHAR(20)
+);
